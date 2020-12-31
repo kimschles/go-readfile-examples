471818a0553a20360a5c7b8af7ce832cf5b143f7
kimschles <schlesinger.kim@gmail.com>
2020-12-31 16:47:19 -0700

    add comments

--- text/main.go
+++ text/main.go
@@ -1,3 +1,5 @@
+// Note: this code and comments are from Chapter 5 of [Head First Go by Jay McGavren](http://headfirstgo.com/)
+
 package main
 
 import (
@@ -8,25 +10,25 @@ import (
 )
 
 func main() {
-
+	// Open the data file for reading
 	file, err := os.Open("example.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// create a new scanner for the file
 	scanner := bufio.NewScanner(file)
 
+	// loops until the end of the file is reached and scanner.Scan returns false
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
 	}
 
+	// Close the file to free resources
 	err = file.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
 
+	// If there was an error scanning the file, report it and exit
 	if scanner.Err() != nil {
 		log.Fatal(scanner.Err())
 	}
-
 }
